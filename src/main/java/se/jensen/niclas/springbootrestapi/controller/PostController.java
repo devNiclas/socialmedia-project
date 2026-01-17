cd76ae46372e94c868cc368c9b7138bcb9d0c400
lauralcodes <charish.weerasuriya@gmail.com>
2026-01-17 18:28:09 +0100

    Remove duplicate post service property

--- src/main/java/se/jensen/niclas/springbootrestapi/controller/PostController.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/controller/PostController.java
@@ -1,6 +1,7 @@
 package se.jensen.niclas.springbootrestapi.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -19,11 +20,6 @@ public class PostController {
 
     private final PostService postService;
     private final List<Post> posts = new ArrayList<>();
-    private final PostService postService;
-
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
 
     public PostController(PostService postService) {
         this.postService = postService;
