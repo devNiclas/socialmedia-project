78e95e1e79b48f41a41719f54419087512be09b8
ssandramedina <ssandramedina@hotmail.com>
2026-01-27 22:57:35 +0100

    till-front-end

--- src/main/java/se/jensen/niclas/springbootrestapi/security/SecurityConfig.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 "http://localhost:5174",
                 "http://localhost:5175",
                 "http://localhost:3000",
-                "https://hissing-delia-sandrasallad-5bbe7e35.koyeb.app",
+                "fragile-sharia-sandrajensen-2281f29e.koyeb.app/",
                 "https://socialmedia-app-project.koyeb.app"
         ));
 
