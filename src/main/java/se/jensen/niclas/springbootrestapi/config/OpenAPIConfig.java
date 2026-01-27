ba2b56439b459abfb91f9b2352070e8a3619c9b0
lauralcodes <charish.weerasuriya@gmail.com>
2026-01-27 11:36:34 +0100

    General documentation off all classes

--- src/main/java/se/jensen/niclas/springbootrestapi/config/OpenAPIConfig.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/config/OpenAPIConfig.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+/**This class don't run a business logic
+ *With annotations it defines API metadata and configures JWT Bearer authentication
+ */
 @OpenAPIDefinition(
         info = @Info(title = "API med JWT", version = "1.0"),
         security = @SecurityRequirement(name = "bearerAuth")
