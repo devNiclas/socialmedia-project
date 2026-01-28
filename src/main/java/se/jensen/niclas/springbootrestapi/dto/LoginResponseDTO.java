a0d74554f7fd21b31fd2c13cfe3f53375aa5ea75
ssandramedina <ssandramedina@hotmail.com>
2026-01-28 21:38:00 +0100

    till-front-end

--- src/main/java/se/jensen/niclas/springbootrestapi/dto/LoginResponseDTO.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/dto/LoginResponseDTO.java
@@ -1,4 +1,11 @@
 package se.jensen.niclas.springbootrestapi.dto;
 
+/**
+ * A record that represents the response after the user has logged in.
+ *
+ * @param token  is used for the users authentication
+ * @param userId the users unique id
+ */
+
 public record LoginResponseDTO(String token, Long userId) {
 }
--- src/main/java/se/jensen/niclas/springbootrestapi/dto/UserRequestDTO.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/dto/UserRequestDTO.java
@@ -3,6 +3,19 @@ package se.jensen.niclas.springbootrestapi.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+/**
+ *
+ * Record of a request when a user is registering their account
+ *
+ * @param username         username needs to have 3 - 30 characters
+ * @param password         password needs to have 6 - 100 characters
+ * @param email            the users own email
+ * @param role             the users role
+ * @param displayName      the users displayed name
+ * @param bio              the bio that will show on the users profile
+ * @param profileImagePath the path of the users profile image
+ */
+
 public record UserRequestDTO(
 
         @NotBlank
@@ -12,7 +25,7 @@ public record UserRequestDTO(
         @NotBlank
         @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters long")
         String password,
-        
+
         String email,
 
         String role,
