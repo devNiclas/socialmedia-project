cc7a15f66c6c567da1e3fa49117329b1a9293831
devNiclas <dev.niclas.b@gmail.com>
2026-01-29 21:02:06 +0100

    Added javadoc for myuserdetailsservice class

--- src/main/java/se/jensen/niclas/springbootrestapi/security/MyUserDetailsService.java
+++ src/main/java/se/jensen/niclas/springbootrestapi/security/MyUserDetailsService.java
@@ -7,21 +7,39 @@ import org.springframework.stereotype.Service;
 import se.jensen.niclas.springbootrestapi.model.User;
 import se.jensen.niclas.springbootrestapi.repository.UserRepository;
 
+/**
+ * Service class that implements UserDetailsService to load user-specific data.
+ * This class is used by Spring Security to retrieve user details during authentication.
+ */
 @Service
 public class MyUserDetailsService implements UserDetailsService {
     private final UserRepository userRepo;
 
+    /**
+     * Constructor for MyUserDetailsService
+     *
+     * @param userRepo the repository used to fetch user data from the database
+     */
     public MyUserDetailsService(UserRepository userRepo) {
         this.userRepo = userRepo;
     }
 
+    /**
+     * Locates the user based on the username.
+     * In this implementation, the user is fetched from the database
+     * And then converted to a UserDetails object.
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a UserDetails object that Spring Security uses for authentication and authorization.
+     * @throws UsernameNotFoundException if the user could not be found.
+     */
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
 
         User user = userRepo.findByUsername(username)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException("Användare hittades inte:" + username));
+                        new UsernameNotFoundException("User not found:" + username));
         return new MyUserDetails(user);
 
     }
